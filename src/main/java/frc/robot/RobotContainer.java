69d8ca0aa04ec69f3f751d3e3973660a42a178b5
aadhyasriram <152321380+aadhyasriram@users.noreply.github.com>
2026-02-12 23:50:50 -0500

    Copied over code from YAMS example, sim currently not working

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,15 +4,33 @@
 
 package frc.robot;
 
+import static edu.wpi.first.units.Units.Meters;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subsystems.Climber;
 
 public class RobotContainer {
+  private final Climber m_climber = new Climber();
+  private final CommandXboxController m_driverController = new CommandXboxController(-1);
   public RobotContainer() {
     configureBindings();
+    // Set the default command to force the elevator to go to 0.
+    m_climber.setDefaultCommand(m_climber.setHeight(Meters.of(0)));
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    // Schedule `setHeight` when the Xbox controller's B button is pressed,
+    // cancelling on release.
+    m_driverController.a().whileTrue(m_climber.setHeight(Meters.of(0.5)));
+    m_driverController.b().whileTrue(m_climber.setHeight(Meters.of(1)));
+    // Schedule `set` when the Xbox controller's B button is pressed,
+    // cancelling on release.
+    m_driverController.x().whileTrue(m_climber.set(0.3));
+    m_driverController.y().whileTrue(m_climber.set(-0.3));
+    
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
